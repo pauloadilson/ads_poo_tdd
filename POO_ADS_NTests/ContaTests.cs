0579eaacca280facc0ec1e8acd635ed6396d78dd
Paulo Adilson da Silva <pauloadilson@gmail.com>
2020-08-27 22:48:14 -0300

    Criação da classe de Teste para TDD da classe Conta.

---
+++ POO_ADS_NTests/ContaTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POO_ADS_N;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ADS_N.Tests
+{
+    [TestClass()]
+    public class ContaTests
+    {
+        [TestMethod()]
+        public void ContaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
