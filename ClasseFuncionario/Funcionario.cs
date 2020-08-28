0a8f7f333d7dd08e11aef208ab36aacbf423292e
Paulo Adilson da Silva <pauloadilson@gmail.com>
2020-08-28 10:50:04 -0300

    Criação da Classe Funcionarios e da classe de testes.

---
+++ ClasseFuncionario/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseFuncionario
+{
+    public class Funcionario
+    {
+        private int codigo;
+        private string nome;
+        private double salario;
+
+        public Funcionario(int codigo = 0, string nome = "", double salario = 0.0)
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.salario = salario;
+        }
+    }
+}
---
+++ ClasseFuncionarioTests/FuncionarioTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClasseFuncionario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseFuncionario.Tests
+{
+    [TestClass()]
+    public class FuncionarioTests
+    {
+        [TestMethod()]
+        public void FuncionarioTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
