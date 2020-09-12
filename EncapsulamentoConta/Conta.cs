c18c63081b47871dd9ff61d2646aa62704fa43ef
Paulo Adilson da Silva <pauloadilson@gmail.com>
2020-09-12 12:17:03 -0300

    Encapsulamento da Classe Conta

---
+++ EncapsulamentoConta/Conta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulamentoConta
+{
+    public class Conta
+    {
+        private int numero;
+        public int Numero
+        {
+            get { return this.numero; }
+            set { this.numero = value; }
+        }
+
+        private string titular;
+        public string Titular
+        {
+            get { return this.titular; }
+            set { this.titular = value; }
+        }
+
+        private double saldo;
+        public double Saldo
+        {
+            get { return this.saldo; }
+            set { this.saldo = value; }
+        }
+
+
+        public Conta(int numero = 0, string titular = "", double saldo = 0.0)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+    }
+}
