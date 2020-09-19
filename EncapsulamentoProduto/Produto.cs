29bbba689a9d816650c3b7ece84857c58b13edb6
Paulo Adilson da Silva <pauloadilson@gmail.com>
2020-09-19 11:39:28 -0300

    Implementação da Classe Produto com encapsulamento

---
+++ EncapsulamentoProduto/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulamentoProduto
+{
+    class Produto
+    {
+        private int codigo;
+        private string descricao;
+        private double preco;
+        public int Codigo
+        {
+            get { return this.codigo; }
+            set { this.codigo = value; }
+        }
+
+        public string Descricao
+        {
+            get { return this.descricao; }
+            set { this.descricao = value; }
+        }
+
+        public double Preco
+        {
+            get { return this.preco; }
+            set { this.preco = value; }
+        }
+
+        public Produto(int codigo = 0, string descricao = "", double preco = 0.0)
+        {
+            this.codigo = codigo;
+            this.descricao = descricao;
+            this.preco = preco;
+        }
+
+        public void Mostrar()
+        {
+            string result = $"\nProduto: {descricao}. Código {codigo}. Preço: {preco.ToString("C")}";
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ EncapsulamentoProduto/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulamentoProduto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Quantos produtos deseja cadastrar?");
+            int nCadastros = Convert.ToInt32(Console.ReadLine());
+            Produto[] produtos = new Produto[nCadastros];
+            for (int i = 0; i < nCadastros; i++)
+            {
+                produtos[i] = new Produto(); //deve-se instanciar cada posicao
+                Console.Write($"\nInforme o código do {i + 1}º produto: ");
+                produtos[i].Codigo = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"Informe a descrição do produto: ");
+                produtos[i].Descricao = Console.ReadLine();
+                Console.Write($"Informe o preço: R$ ");
+                produtos[i].Preco = Convert.ToDouble(Console.ReadLine());
+            }
+            double somaProdutos = 0;
+            foreach (Produto p in produtos)
+            {
+                somaProdutos += p.Preco;
+                p.Mostrar();
+            }
+            Console.WriteLine($"\nA soma dos produtos registrados é R$ {somaProdutos:C}.");
+            Console.ReadKey();
+
+        }
+    }
+}
