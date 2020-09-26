b931b9b58ca319b160571909020af0f90c66a085
Paulo Adilson da Silva <pauloadilson@gmail.com>
2020-09-26 12:09:44 -0300

    Classes Conta e Clientes agregadas

---
+++ AgregacaoConta/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgregacaoConta
+{
+    class Cliente
+    {
+        private string nome;
+        private string rg;
+        private string cpf;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Rg
+        {
+            get { return rg; }
+            set { rg = value; }
+        }
+        public string Cpf
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine($"Nome: {Nome}\tRg: {Rg}\tCpf: {Cpf}");
+        }
+    }
+}
---
+++ AgregacaoConta/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgregacaoConta
+{
+    class Conta
+    {
+        private int numero;
+        private Cliente titular;
+        private double saldo;
+
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+
+        public Cliente Titular
+        {
+            get { return titular; }
+            set { titular = value; }
+        }
+
+        public double Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine($"Numero: {Numero}\tSaldo: {Saldo}");
+            this.Titular.Mostrar();
+        }
+    }
+}
