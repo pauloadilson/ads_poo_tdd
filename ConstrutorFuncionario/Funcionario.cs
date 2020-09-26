3b02f92807f7f86cc372ed99e07b8f0ad12bf1e9
Paulo Adilson da Silva <pauloadilson@gmail.com>
2020-09-26 11:46:38 -0300

    Implementação dos construtores da classe Funcionário

---
+++ ConstrutorFuncionario/Funcionario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrutorFuncionario
+{
+    class Funcionario
+    {
+        private int codigo;
+        private string nome;
+        private double salario;
+        private static int contador;
+
+        public static int Contador
+        {
+            get { return contador; }
+        }
+
+        static Funcionario()
+        {
+            contador = 99;
+        }
+
+        public Funcionario()
+        {
+            contador++;
+        }
+
+        public Funcionario(int codigo, double salario)
+        {
+            contador++;
+            this.codigo = codigo;
+            this.salario = salario;
+        }
+
+        public Funcionario(int codigo, string nome, double salario)
+        {
+            contador++;
+            this.codigo = codigo;
+            this.nome = nome;
+            this.salario = salario;
+        }
+
+        public void Mostrar()
+        {
+            string result = $"Funcionário: {nome}. Código {codigo}. Salário: {salario.ToString("C")}";
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ConstrutorFuncionario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrutorFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario c1 = new Funcionario();//utilização para cadastro
+            c1.Mostrar();
+
+            Funcionario c2 = new Funcionario(123,2000.0);
+            c2.Mostrar();
+
+            Funcionario c3 = new Funcionario(199, "Ana", 1005.0);//pesquisa de um banco
+            c3.Mostrar();
+
+            Console.WriteLine($"A quantidade de instâncias é {Funcionario.Contador}");
+
+            Console.ReadKey();
+        }
+    }
+}
